2915ee1f458c4c299a9dc2d9a430aebf38431866
sumbalfida <sumbalfida700@gmail.com>
2025-12-19 22:22:10 +0500

    Update script.js

--- script.js
+++ script.js
@@ -1,19 +1,19 @@
 // Smooth scroll & active link highlight
 const links = document.querySelectorAll('#sidebar nav ul li a');
-links.forEach(link => {
-    link.addEventListener('click', e => {
+links.forEach(link=>{
+    link.addEventListener('click', e=>{
         e.preventDefault();
         document.querySelector(link.getAttribute('href')).scrollIntoView({behavior:'smooth'});
     });
 });
 
-window.addEventListener('scroll', () => {
+window.addEventListener('scroll',()=>{
     let fromTop = window.scrollY + 100;
-    links.forEach(link => {
-        const section = document.querySelector(link.getAttribute('href'));
-        if(section.offsetTop <= fromTop && section.offsetTop + section.offsetHeight > fromTop){
+    links.forEach(link=>{
+        const section=document.querySelector(link.getAttribute('href'));
+        if(section.offsetTop<=fromTop && section.offsetTop+section.offsetHeight>fromTop){
             link.classList.add('active');
-        } else {
+        }else{
             link.classList.remove('active');
         }
     });
